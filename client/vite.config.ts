9b9f63ba073ff2848bf529e3169ed910bc801115
pranavmalwatkar <130284009+pranavmalwatkar@users.noreply.github.com>
2025-08-18 23:30:43 +0530

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true,
-        secure: false,
+        secure: false, // Set to true only if your backend uses HTTPS
       },
     },
   },
